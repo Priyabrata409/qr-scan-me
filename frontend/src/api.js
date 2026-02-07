aa9c9934c0951b33fbc5f4c9e6d9374c8ef930d5
Priyabrata Panda <priyabrata.panda@coupa.com>
2026-02-08 02:16:37 +0530

    First commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,7 @@
-import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
 const api = axios.create({
-    baseURL: API_URL,
+    baseURL: API_URL.startsWith('http') ? API_URL : `https://${API_URL}`,
 });
 
 api.interceptors.request.use((config) => {
